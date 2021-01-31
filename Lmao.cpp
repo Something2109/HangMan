25111a7956a238bc94cdd8b44db903c645efe448
Something2109 <75402447+Something2109@users.noreply.github.com>
2021-01-31 22:19:55 +0700

    Update Lmao.cpp

--- Lmao.cpp
+++ Lmao.cpp
@@ -11,16 +11,3 @@ int main() {
 	menu();
 	return 0;
 }
-
-/*int main() {
-	int wrongGuess;
-	cin >> wrongGuess;
-	cout << "________" << endl;
-	cout << '|' << setw(7) << ((wrongGuess >= 1) ? '|' : ' ' ) << endl;
-	cout << '|' << setw(7) << ((wrongGuess >= 2) ? 'O' : ' ') << endl;
-	cout << '|' << setw(6) << ((wrongGuess >= 4) ? '/' : ' ') << ((wrongGuess >= 3) ? '|' : ' ') << ((wrongGuess >= 5) ? '\\' : ' ') << endl;
-	cout << '|' << setw(6) << ((wrongGuess >= 6) ? '/' : ' ') << ((wrongGuess == 7) ? " \\" : " " ) << endl;
-	cout << '|' << endl;
-	cout << '|' << endl;
-	cout << "|_______" << endl;
-}*/
